31f15fb2624882c2a29728322837e1597c8f859e
Eric Bernard <eric.bernard@aurora.app>
2020-03-12 14:20:52 +0300

    fetch and display authors

---
+++ src/components/addBook.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const getAuthorsQuery = gql`
+  {
+    authors {
+      name
+      age
+      id
+    }
+  }
+`;
+
+const AddBookList = props => {
+  const { loading, error, data } = useQuery(getAuthorsQuery);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  const { authors } = data;
+
+  console.log("the authors", authors);
+
+  return (
+    <div>
+      <form id="add-book">
+        <div className="field">
+          <label>Book name:</label>
+          <input type="text" />
+        </div>
+        <div className="field">
+          <label>Genre:</label>
+          <input type="text" />
+        </div>
+        <div className="field">
+          <label>Author:</label>
+          <select>
+            <option>Select author</option>
+            {authors.map(author => (
+              <option key={author.id} value={author.id}>
+                {author.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button>+</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddBookList;
