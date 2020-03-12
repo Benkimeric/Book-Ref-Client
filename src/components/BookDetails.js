44c7c988baaabed26c14aaa2b28649ab542e3002
Eric Bernard <eric.bernard@aurora.app>
2020-03-12 16:54:53 +0300

    implement fetch one and view book details

---
+++ src/components/BookDetails.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+
+import { getBookQuery } from "../queries/queries";
+
+const BookDetails = props => {
+  const { loading, error, data } = useQuery(getBookQuery, {
+    variables: { id: props.bookId }
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  const { book } = data;
+  console.log("data", book);
+  const displayBook = () => {
+    if (book) {
+      return (
+        <div>
+          <h2>{book.name}</h2>
+          <p>{book.genre}</p>
+          <p>{book.author.name}</p>
+          <p>All books by this author</p>
+          <ul className="other-books">
+            {book.author.books.map(book => (
+              <li key={book.id}>{book.name}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    } else {
+      return <div>No book selected</div>;
+    }
+  };
+  return (
+    <div id="book-details">
+      <p>Output book details here </p>
+      {displayBook()}
+    </div>
+  );
+};
+
+export default BookDetails;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/react-hooks";
 
 import { getBooksQuery } from "../queries/queries";
+import BookDetails from "./BookDetails";
 
 const BookList = props => {
   const { loading, error, data } = useQuery(getBooksQuery);
+  const [state, setState] = useState("");
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
   const { books } = data;
 
+  const handleBookClick = book => {
+    setState(book.id);
+  };
+
   return (
     <div>
       <ul id="book-list">
         {books.map((book, idx) => (
-          <li key={idx}>{book.name}</li>
+          <li key={idx} onClick={() => handleBookClick(book)}>
+            {book.name}
+          </li>
         ))}
       </ul>
+      <BookDetails bookId={state} />
     </div>
   );
 };
