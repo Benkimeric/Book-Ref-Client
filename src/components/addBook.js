47e53bc74d0743a8fb1732341da88325b17b6354
Eric Bernard <eric.bernard@aurora.app>
2020-03-12 15:56:29 +0300

    add new book and refetch details

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -1,31 +1,61 @@
-import React from "react";
-import { useQuery } from "@apollo/react-hooks";
+import React, { useState } from "react";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 
-import { getAuthorsQuery } from "../queries/queries";
+import {
+  getAuthorsQuery,
+  getBooksQuery,
+  addBookMutation
+} from "../queries/queries";
 
 const AddBookList = props => {
+  const initialState = {
+    name: "",
+    authorId: "",
+    genre: ""
+  };
+
+  const [state, setState] = useState(initialState);
+
+  const handleChange = e => {
+    setState({
+      ...state,
+      [e.target.id]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addBook();
+  };
+
   const { loading, error, data } = useQuery(getAuthorsQuery);
+  const [addBook] = useMutation(addBookMutation, {
+    variables: {
+      name: state.name,
+      authorId: state.authorId,
+      genre: state.genre
+    },
+    refetchQueries: [{ query: getBooksQuery }]
+  });
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
   const { authors } = data;
 
-  console.log("the authors", authors);
-
   return (
     <div>
-      <form id="add-book">
+      <form id="add-book" onSubmit={handleSubmit}>
         <div className="field">
           <label>Book name:</label>
-          <input type="text" />
+          <input id="name" onChange={handleChange} type="text" />
         </div>
         <div className="field">
           <label>Genre:</label>
-          <input type="text" />
+          <input id="genre" onChange={handleChange} type="text" />
         </div>
         <div className="field">
           <label>Author:</label>
-          <select>
+          <select id="authorId" onChange={handleChange}>
             <option>Select author</option>
             {authors.map(author => (
               <option key={author.id} value={author.id}>
